63a20f7a1e6de8047f4a9ef1cc9678bf68e2a991
Railven <railven@inbox.ru>
2017-08-05 22:21:20 +0300

    testProductStickers

---
+++ src/test/java/TestProductStickers.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+public class TestProductStickers extends BaseTest {
+    @Test
+    public void testMenuHeaders() {
+        driver.get("http://localhost/litecart/");
+        List<WebElement> products = driver.findElements(By.className("product"));
+        for (int i = 0; i < products.size(); i++ ) {
+            products = driver.findElements(By.className("product"));
+            List<WebElement> stickers = products.get(i).findElements(By.className("sticker"));
+            Assert.assertNotNull(stickers, "У данного товара стикер отсутствует");
+            Assert.assertEquals(
+                    stickers.size(),
+                    1,
+                    "Количество стикеров у товара не равно 1"
+            );
+        }
+    }
+}
